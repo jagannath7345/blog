065fda323151460c4beb90a7c9ff258aa29aeb57
Jagannath <guptajn7345@gmail.com>
2023-08-02 08:12:20 +0530

    fix db error

--- index.js
+++ index.js
@@ -7,13 +7,19 @@ const blogRoutes = require("./routes/blogRoutes");
 const blog = require("./models/Blog");
 const { checkAuthentication } = require("./middleware/authntication");
 const app = express();
+require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect("mongodb://127.0.0.1:27017/blogfy").then(() => {
-  console.log("database connected sucessfully");
-});
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log("database connected sucessfully");
+  })
+  .catch((err) => {
+    console.log("connection failed");
+  });
 app.set("view engine", "ejs");
-app.use(express.static(path.resolve("./public")))
+app.use(express.static(path.resolve("./public")));
 app.set("views", path.resolve("./views"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
